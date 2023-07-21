44b2762ac27ea6d39f3fcbc03c0d794c742ea7a8
Atte Saarela <attesaarela@gmail.com>
2023-07-21 05:04:29 +0300

    minor cleanup

--- model.py
+++ model.py
@@ -72,7 +72,7 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.gelu = nn.GELU()
+        self.gelu    = nn.GELU()
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
